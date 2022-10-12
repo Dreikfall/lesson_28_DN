6a4bf0e0df1323720fac00b04b0cd4fc10d7056b
Dreikfall <Dreikfall@yandex.ru>
2022-10-12 10:12:15 +0400

    user_update commit

--- users/urls.py
+++ users/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.UserListView.as_view()),
     path('create/', views.UserCreateView.as_view()),
     path('delete/', views.UserDeleteView.as_view()),
-    path('<int:pk>/', views.UserDetailView.as_view())
+    path('<int:pk>/', views.UserDetailView.as_view()),
+    path('<int:pk>/update/', views.UserUpdateView.as_view())
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 import json
 
 from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
@@ -28,7 +29,7 @@ class UserListView(ListView):
                     "password": user.password,
                     "role": user.role,
                     "age": user.age,
-                    #"total_ads": user.ads.count() # в разборе работало, здесь нет((
+                    "total_ads": user.ad_set.count()
                 })
         return JsonResponse(users, safe=False)
 
@@ -80,6 +81,41 @@ class UserCreateView(CreateView):
                              'age': user.age,
                              'locations': [str(u) for u in user.location.all()]
                              })
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class UserUpdateView(UpdateView):
+    model = User
+    fields = ['username', 'first_name', 'last_name',
+              'password', 'role', 'locations', 'age']
+
+    def patch(self, request, *args, **kwargs):
+        super().post(request, *args, **kwargs)
+        data = json.loads(request.body)
+
+
+        locations = get_object_or_404(Location, id=data['locations'])
+
+        self.object.username = data['username']
+        self.object.first_name = data['first_name']
+        self.object.last_name = data['last_name']
+        self.object.role = data['role']
+        self.object.password = data['password']
+        self.object.age = data['age']
+        self.object.locations = locations
+        self.object.save()
+        return JsonResponse(
+            {
+                "id": self.object.id,
+                "username": self.object.username,
+                "first_name": self.object.first_name,
+                "last_name": self.object.last_name,
+                "role": self.object.role,
+                "password": self.object.password,
+                "age": self.object.age,
+                "locations": # вот здесь я встрял
+            }
+         )
 
 
 @method_decorator(csrf_exempt, name='dispatch')
